9465efca722f354a712b91c27be05a3f76b7cb2a
Rossy <rossyesmeral@gmail.com>
2022-07-01 13:13:38 -0400

    funcionalidad hide y eventos cDOM primera carta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const addition = document.getElementById('addition');
+const subtraction = document.getElementById('subtraction');
+const quantity = document.getElementById('quantity');
+
+//const buyBtn = document.getElementById('buy');
+//const cardSecond = document.getElementById('cardSecond');
+
+addition.addEventListener('click', addQuantity);
+subtraction.addEventListener('click', subQuantity);
+
+function addQuantity() {
+    let quantityProduct = parseInt(quantity.textContent);
+    quantityProduct++;
+    quantity.innerText = quantityProduct;
+    multiplyAmount(quantityProduct);
+}
+
+function subQuantity() {
+    let quantityProduct = parseInt(quantity.textContent);
+    if (quantityProduct === 1) {
+        return;
+    }
+    quantityProduct--;
+    quantity.innerText = quantityProduct;
+    multiplyAmount(quantityProduct)
+}
+
+function multiplyAmount(quantity) {
+    const amount = document.getElementById('amount');
+    let amountValue = parseInt(amount.textContent);
+    amount.innerText = quantity * amountValue;
+}
+
+/*buyBtn.addEventListener('click', buycomprar);
+
+function buycomprar() {
+    buyBtn.classList.add("cardSecond");
+    cardQuestion.classList.remove('cardFirst');
+    
+}*/
+
+
+
+
+
+
+
+
+
+
