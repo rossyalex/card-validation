1a4f7d1587d4799110cff66b049d943b39eb8be9
Rossy <rossyesmeral@gmail.com>
2022-07-04 21:29:48 -0400

    arreglar mi funtion multiplyAmount y buy para tener el precio a pagar

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ console.log(validator);
 const addition = document.getElementById('addition');
 const subtraction = document.getElementById('subtraction');
 const quantity = document.getElementById('quantity');
+const amount = document.getElementById('amount');
 const buyBtn = document.getElementById('buy');
 const backBtn = document.getElementById('back');
 const finalizeBtn = document.getElementById('finalize');
 const payBtn = document.getElementById('pay');
 const cardFirst = document.getElementById('card1');
-const cardSecond = document.getElementById('card2'); 
+const cardSecond = document.getElementById('card2');
+const purchaseAmount = document.getElementById('purchaseAmount');
 const cardThird = document.getElementById('card3');
 const cardQuarter = document.getElementById('card4');
+
 
 
 addition.addEventListener('click', addQuantity);
@@ -41,33 +44,32 @@ function subQuantity() {
 }
 
 function multiplyAmount(quantity) {
-    const amount = document.getElementById('amount');
-    let amountValue = parseInt(amount.textContent);
-    amount.innerText = quantity * amountValue;
+    const priceProduct = 600;
+    amount.innerText =  (quantity * priceProduct).toString();
 }
 
 function buy() {
-    cardFirst.classList.add('hide'); 
-    cardSecond.classList.remove('hide'); 
+    cardFirst.classList.add('hide');
+    cardSecond.classList.remove('hide');
+    purchaseAmount.innerText = amount.textContent 
 }
 
 function back() {
-    cardSecond.classList.add('hide'); 
-    cardFirst.classList.remove('hide'); 
+    cardSecond.classList.add('hide');
+    cardFirst.classList.remove('hide');
 }
-
 
 
 function finalize() {
     cardSecond.classList.add('hide');
-    cardThird.classList.remove('hide'); 
-     
+    cardThird.classList.remove('hide');
+
 }
 
 function pay() {
     cardThird.classList.add('hide');
     cardQuarter.classList.remove('hide');
-      
+
 }
 
 
